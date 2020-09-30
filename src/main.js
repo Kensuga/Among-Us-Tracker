e0b7703e4d433e3952edc4091fa3f3639d957a73
Kensuga <cruzlmcastillo@gmail.com>
2020-09-29 23:29:31 -0700

    First  Commit

--- src/main.js
+++ src/main.js
@@ -1,8 +1,45 @@
-import Vue from "vue";
-import App from "./App.vue";
+import Vue from 'vue';
+import App from './App.vue';
+
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import {
+	faPlus,
+	faMinus,
+	faEdit,
+	faTrash,
+	faSave,
+	faCheck,
+	faSyncAlt,
+	faMinusCircle,
+} from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
+import Vuetify from 'vuetify';
+import 'vuetify/dist/vuetify.min.css';
+
+Vue.use(Vuetify);
+
+library.add(
+	faPlus,
+	faMinus,
+	faEdit,
+	faTrash,
+	faSave,
+	faCheck,
+	faSyncAlt,
+	faMinusCircle
+);
+
+Vue.component('font-awesome-icon', FontAwesomeIcon);
 
 Vue.config.productionTip = false;
+Vue.use(BootstrapVue);
+Vue.use(IconsPlugin);
+
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
 
 new Vue({
-  render: h => h(App)
-}).$mount("#app");
+	render: (h) => h(App),
+}).$mount('#app');
